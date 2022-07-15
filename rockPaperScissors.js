39bcc259f04c46070b9e897169d36146a1611a8c
Capz-DH <108783601+Capz-DH@users.noreply.github.com>
2022-07-15 16:08:05 +0100

    Add files via upload

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,15 +1,41 @@
-const rock = document.querySelector(".rock");
-const paper = document.querySelector(".paper");
-const scissors = document.querySelector(".scissors");
+function computerPlay(){
+    const ranNum = Math.floor(Math.random() * 3);
+    const choices = ["Rock", "Paper", "Scissors"];
+    return choices[ranNum];
+};
 
-rock.addEventListener("click", e => {
-	console.log(e.target.className);
-});
+const button1 = document.querySelector('.Rock'); 
+button1.addEventListener('click', playerPlay);
 
-paper.addEventListener("click", e => {
-	console.log(e.target.className);
-});
+const button2 = document.querySelector('.Paper'); 
+button2.addEventListener('click', playerPlay);
 
-scissors.addEventListener("click", e => {
-	console.log(e.target.className);
-});
+const button3 = document.querySelector('.Scissors'); 
+button3.addEventListener('click', playerPlay);
+
+
+function playerPlay(event){
+   console.log('working');
+}
+
+
+function playRound(playerSelection, computerSelection)  {
+    
+    computerSelection = computerPlay();
+    playerSelection = playerPlay();
+ 
+    if (computerSelection === "Rock" && playerSelection === "Scissors"){
+        return "You Lose! Rock beats Scissors!";
+    }  else if (computerSelection === "Rock" && playerSelection === "Paper"){
+        return "You Win! Paper beats Rock!";
+    }  else if (computerSelection === "Scissors" && playerSelection === "Paper"){
+        return "You lose! Scissors beats Paper!";
+    }  else if (computerSelection === "Scissors" && playerSelection === "Rock"){
+        return "You Win! Rock beats Scissors!";
+    }  else if (computerSelection === "Paper" && playerSelection === "Scissors"){
+        return "You Win! Scissors beats Paper!";
+    }  else if (computerSelection === "Paper" && playerSelection === "Rock"){
+        return "You lose! Rock beats paper!";
+    }  else if (computerSelection === playerSelection)
+        return "Draw!";
+}
